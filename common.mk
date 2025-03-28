3f6134e467945b0550e324572a21c178b0ea85d3
absoluten00b <whiteviolent8@gmail.com>
2025-03-28 12:18:47 +0000

    Revert "sm8350-common: Inherit oplus Camera"
    This reverts commit b74511fb8ff23b0623bd55f5eacbb3ee56f0fd33.

--- common.mk
+++ common.mk
@@ -94,7 +94,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
-$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
